3e65f5c1f0c7808fe5854ff1ed8f9a11e7b90617
GIANG THAO <thaogiang.90@gmail.com>
2021-12-01 11:56:06 +0700

    Handle next element in table user next()

--- cypress/integration/examples/TC_06_Handle_Child_Tab.js
+++ cypress/integration/examples/TC_06_Handle_Child_Tab.js
@@ -1,10 +1,28 @@
 /// <reference types="Cypress" />
 describe('My Second Example Test Case', function () {
-    it('My Third Test Case', function () {
-        cy.visit("https://www.rahulshettyacademy.com/AutomationPractice/");
-        //Control Child Tab
-        cy.get('#opentab').invoke('removeAttr','target').click()
-     
+  it('My Third Test Case', function () {
+    cy.visit("https://www.rahulshettyacademy.com/AutomationPractice/");
+    //Control Child Tab
+    cy.get('#opentab').invoke('removeAttr', 'target').click()
+    // Verify The Url correct
+    cy.url().should('include', 'https://www.rahulshettyacademy.com/#/index')
+    cy.wait(10000)
+    //back to origin url
+    cy.go('back')
+    //Code list all element then for all element
+    cy.get('tr td:nth-child(2)').each(($el, index, $list) => {
+      const textVeg = $el.text();
+      if (textVeg.includes('simple Python Language')) {
+        //Code use next element sibling
+        cy.get('tr td:nth-child(2)').eq(index).next().then(function (price) {
+          const priceExpect = price.text()
+          expect(priceExpect).to.equal("25")
+        })
+      }
     })
+
+
+
+  })
 }
 )
