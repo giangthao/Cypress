051f00a5e04e7a34d276e9b546f2b9c073867eae
giangthao <thaogiang.90@gmail.com>
2021-12-03 22:21:00 +0700

    Fix pretty code Handle Iframe

--- cypress/integration/examples/TC_09_Handle_Ifram.js
+++ cypress/integration/examples/TC_09_Handle_Ifram.js
@@ -3,9 +3,9 @@ import 'cypress-iframe';
 describe('My Second Example Test Case', function () {
   it('My Third Test Case', function () {
     cy.visit("https://www.rahulshettyacademy.com/AutomationPractice/");
-   cy.frameLoaded('#courses-iframe')
-   cy.iframe().find("a[href='#/mentorship']").eq(0).click()
-  cy.iframe().find('h1[class="pricing-title text-white ls-1"]').should('have.length',2)
+    cy.frameLoaded('#courses-iframe')
+    cy.iframe().find("a[href='#/mentorship']").eq(0).click()
+    cy.iframe().find('h1[class="pricing-title text-white ls-1"]').should('have.length', 2)
 
   })
 }
