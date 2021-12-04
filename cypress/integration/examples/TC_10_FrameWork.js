e4f423b7114e68a7bd0a1ebb0190cc12343dc880
giangthao <thaogiang.90@gmail.com>
2021-12-04 09:37:17 +0700

    How to connect data from Json File

--- cypress/integration/examples/TC_10_FrameWork.js
+++ cypress/integration/examples/TC_10_FrameWork.js
@@ -2,12 +2,19 @@
 import 'cypress-iframe';
 describe('My Ten Test Case', function () {
     before(() => {
-        // runs once before all tests in the block
+       cy.fixture('example').then(function(data){
+        this.data=data
+       })
       })
-  it('My Third Test Case', function () {
-    cy.visit("https://rahulshettyacademy.com/angularpractice/");
-    cy.get('input[name="name"]:nth-child(2)').type('Thao')
+  it('My First Test Case', function () {
+    cy.visit('https://rahulshettyacademy.com/angularpractice/')
+    cy.get('input[name="name"]:nth-child(2)').type(this.data.name)
+    cy.get('select').select(this.data.gender)
+ 
 
+  })
+  it('My Second Test Case', function () {
+    cy.get('input[name="name"]:nth-child(2)').type('Thao 02')
   })
 }
 )
