89a1f54288051d3aa42a89bc209d428d4f359c75
giangthao <thaogiang.90@gmail.com>
2021-12-06 23:35:31 +0700

    Update dynamic enviroment

--- cypress/integration/examples/TC_13_HomePage.js
+++ cypress/integration/examples/TC_13_HomePage.js
@@ -11,7 +11,7 @@ describe('My Ten Test Case', function () {
       })
   it('My First Test Case', function () {
     
-    cy.visit('https://rahulshettyacademy.com/angularpractice/')
+    cy.visit(Cypress.env('url'))
     const homePage = new HomePage()
     const productPage = new ProductPage()
     homePage.getNameTextbox().type(this.data.name)
@@ -29,13 +29,33 @@ describe('My Ten Test Case', function () {
       cy.selectProduct(element)
     })
     productPage.getCheckOutButton().click()
+    var sum =0
+    cy.get('tr td:nth-child(4) strong').each(($el, index, list)=>{
+      const result = $el.text()
+      var rel;
+      rel = parseFloat(result.substring(3))
+   
+      sum = sum + rel
+  
+    })
+    cy.get('h3 strong').then(function(element){
+      var actual;
+      actual = parseFloat(element.text().substring(3))
+      expect(Number(sum)).to.equal(Number(actual))
+    })
     cy.contains('Checkout').click()
+    
     cy.get('#country').type('India')
     cy.get(".suggestions > ul > li > a").click()
     cy.get('#checkbox2').click({force: true})
     cy.get('input[type="submit"]').click()
     cy.get('div.alert').contains('Thank you! Your order will be delivered in next few weeks :-).')
- 
+    cy.get('div.alert').then(function(element)
+    {
+        const actual = element.text()
+        expect(actual.includes('Success')).to.true
+    }
+    )
 
 
 
